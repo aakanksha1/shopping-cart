03f805c7fa7021aeb6b021020c845bd565f22164
Aakanksha Ardhapurkar <aakanksha720@gmail.com>
2020-01-20 22:38:40 -0600

    deleting all items from the cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Navbar, Nav, Dropdown, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { CartContext } from './CartProvider';
 
 const ShoppingCart = () => {
@@ -7,6 +9,25 @@ const ShoppingCart = () => {
 	const { cart, setCart } = useContext(
 		CartContext
 	);
+
+	const deleteItem = product => {
+		const emptyingCart = cart.filter(shoppingItem => product.id !== shoppingItem.id);
+		setCart(emptyingCart);
+	}
+
+	const CheckoutItem = ({ product }) => {
+		return (
+			<ListGroupItem className="card-container">
+				<FontAwesomeIcon icon={faTimes} 
+					onClick={() => deleteItem(product)}
+				/>
+				<img src={`data/products/${product.sku}_2.jpg`} alt="" />
+				<p>{product.title}</p>
+				<p>${product.price}</p>
+				<p>Size:{product.size}</p>
+			</ListGroupItem>
+		)
+	}
 
 	return (
 		<Navbar bg="dark" variant="dark" className="navigation-bar">
@@ -22,12 +43,7 @@ const ShoppingCart = () => {
 						<hr />
 						{cart.map(product => (
 							<ListGroup>
-								<ListGroupItem className="card-container">
-									<img src={`data/products/${product.sku}_2.jpg`} alt="" />
-									<p>{product.title}</p>
-									<p>{`$${product.price}`}</p>
-									<p>Size: {product.size}</p>
-								</ListGroupItem>
+								<CheckoutItem product = {product}/>
 								<br></br>
 							</ListGroup>
 						))}
